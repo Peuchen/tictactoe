c7e16df0abc0a5001eaee034d018e41311e66568
peuchen <npeuchen@gmail.com>
2020-10-15 21:21:35 +0200

    Add Player class and initialize method

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,20 @@
-#Player
 -# Create a class for the players
-#- Create a initialize method which includes a symbol to play with
+class Player
+  @@player_count = 0
+
+  #- Create a initialize method which includes a symbol to play with
+  def initialize
+    @symbol = symbol
+    if @@player_count = 0
+      symbol = :x
+    else
+      symbol = :o
+    end
+    @@player_count += 1
+  end
+
+end
+
 #- Create a method that allows a player to place a symbol on the playing board (the place on the playing board is the argument) 
 #	- Ask the players for input (gets.chomp)
 #	- Substitute one space on the board with the symbol
