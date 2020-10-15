71330487410914817e73a41b0144b1803d09fe83
peuchen <npeuchen@gmail.com>
2020-10-15 21:19:44 +0200

    Add pseudocode

---
+++ tictactoe.rb
@@ -0,0 +1,11 @@
+#Player
+-# Create a class for the players
+#- Create a initialize method which includes a symbol to play with
+#- Create a method that allows a player to place a symbol on the playing board (the place on the playing board is the argument) 
+#	- Ask the players for input (gets.chomp)
+#	- Substitute one space on the board with the symbol
+#	- Create a new method that, after placing each symbol, checks whether the current player has three symbols in a row
+
+#PlayingBoard
+#- Create a class for the playing board
+#- Create a initialize method that sets up the board
