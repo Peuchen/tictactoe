065d99843e26f7eb99704e18a68e5b18641455f6
peuchen <npeuchen@gmail.com>
2020-11-02 19:23:29 +0100

    Refactor methods

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,17 +1,16 @@
 class Game
-  attr_accessor :board
 
-  $win = false
   def initialize
     $board = "1 2 3\n4 5 6\n7 8 9"
     @player1 = Player.new
-    puts "Player 1 has the symbol #{@player1.symbol}"
     @player2 = Player.new
-    puts "Player 2 has the symbol #{@player2.symbol}"
   end
 
   def play
+    puts "Player 1 has the symbol #{@player1.symbol} and may start."
+    puts "Player 2 has the symbol #{@player2.symbol}."
     puts $board
+
     loop do
       @player1.place_symbol
       if @player1.has_won?
@@ -21,35 +20,32 @@ class Game
         puts "You have tied!"
         break
       end
+
       @player2.place_symbol
       if @player2.has_won?
         puts "Player 2, you have won!"
         break
       end
+
     end
   end
 end
 
-# Create a class for the players
 class Player
   attr_reader :symbol, :name
   @@player_count = 0
 
-  #- Create a initialize method which includes a symbol to play with
   def initialize
     if @@player_count == 0
       @name = "Player 1"
       @symbol = :X
-    elsif @@player_count == 1
+    else
       @name = "Player 2"
       @symbol = :O
-    else
-      puts "Tic Tac Toe can only be played by two players"
     end
     @@player_count += 1
   end
 
-  #- Create a method that allows a player to place a symbol on the playing board (the place on the playing board is the argument) 
   def place_symbol
     puts "#{self.name}, at what position do you want place to place an #{self.symbol}?"
     position = gets.chomp
@@ -57,28 +53,27 @@ class Player
       puts "You have not indicated an available position. Please try again."
       position = gets.chomp
     end
-    #	- Substitute one space on the board with the symbol
+
     $board = $board.sub(position.to_s, self.symbol.to_s)
     puts $board
-    #	- Create a new method that, after placing each symbol, checks whether the current player has three symbols in a row, by using the index numbers of the string $board
   end
 
   def has_won?
-    if ($board[8] == self.symbol.to_s) && ($board[0] == self.symbol.to_s) && ($board[16] == self.symbol.to_s)
+    if ($board[0] == $board[2]) && ($board[0] == $board[4])
       return true
-    elsif ($board[8] == self.symbol.to_s) && ($board[4] == self.symbol.to_s) && ($board[12] == self.symbol.to_s)
+    elsif ($board[0] == $board[6]) && ($board[0] == $board[12])
       return true
-    elsif ($board[8] == self.symbol.to_s) && ($board[6] == self.symbol.to_s) && ($board[10] == self.symbol.to_s)
+    elsif ($board[8] == $board[0]) && ($board[8] == $board[16])
       return true
-    elsif ($board[8] == self.symbol.to_s) && ($board[2] == self.symbol.to_s) && ($board[14] == self.symbol.to_s)
+    elsif ($board[8] == $board[2]) && ($board[8] == $board[14])
       return true
-    elsif ($board[0] == self.symbol.to_s) && ($board[2] == self.symbol.to_s) && ($board[4] == self.symbol.to_s)
+    elsif ($board[8] == $board[4]) && ($board[8] == $board[12])
       return true
-    elsif ($board[0] == self.symbol.to_s) && ($board[6] == self.symbol.to_s) && ($board[12] == self.symbol.to_s)
+    elsif ($board[8] == $board[6]) && ($board[8] == $board[10])
       return true
-    elsif ($board[16] == self.symbol.to_s) && ($board[4] == self.symbol.to_s) && ($board[10] == self.symbol.to_s)
+    elsif ($board[16] == $board[4]) && ($board[16] == $board[10])
       return true
-    elsif ($board[16] == self.symbol.to_s) && ($board[12] == self.symbol.to_s) && ($board[14] == self.symbol.to_s)
+    elsif ($board[16] == $board[12]) && ($board[16] == $board[14])
       return true
     else
       return false
